906bc9875c7d5724ec1a34a1f3e4a4da5cd866e0
itoukouta <itokouta8250@icloud.com>
2025-10-07 15:22:12 +0900

    Remove node_modules from repo

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import styles from "./Header.module.css";
+export default function Header() {
+  return (
+    <>
+      <header className={styles.header}>
+        <ul className={styles.headerMenu}>
+          <li>
+            <a href="">Blog</a>
+          </li>
+          <li>
+            <a href="">お問い合わせ</a>
+          </li>
+        </ul>
+      </header>
+    </>
+  );
+}
