072b3b1544a54f3313e8cad867b5946fbb70163b
itoukouta <itokouta8250@icloud.com>
2025-10-07 17:14:42 +0900

    Fragment delete

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -2,17 +2,15 @@ import React from "react";
 import styles from "./Header.module.css";
 export default function Header() {
   return (
-    <>
-      <header className={styles.header}>
-        <ul className={styles.headerMenu}>
-          <li>
-            <a href="">Blog</a>
-          </li>
-          <li>
-            <a href="">お問い合わせ</a>
-          </li>
-        </ul>
-      </header>
-    </>
+    <header className={styles.header}>
+      <ul className={styles.headerMenu}>
+        <li>
+          <a href="">Blog</a>
+        </li>
+        <li>
+          <a href="">お問い合わせ</a>
+        </li>
+      </ul>
+    </header>
   );
 }
