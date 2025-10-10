3c9dfc79db5f55853369b73e487211eb342e8884
itoukouta <itokouta8250@icloud.com>
2025-10-11 08:53:37 +0900

    loading　管理

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -4,18 +4,28 @@ import { useEffect, useState } from "react";
 
 export default function Home() {
   const [posts, setPosts] = useState([]);
-
+  const [loading, setLoading] = useState(true);
   useEffect(() => {
     const fetcher = async () => {
-      const res = await fetch(
-        "https://1hmfpsvto6.execute-api.ap-northeast-1.amazonaws.com/dev/posts"
-      );
-      const data = await res.json();
-      setPosts(data.posts);
+      try {
+        const res = await fetch(
+          "https://1hmfpsvto6.execute-api.ap-northeast-1.amazonaws.com/dev/posts"
+        );
+        const data = await res.json();
+        setPosts(data.posts);
+      } catch (error) {
+        console.error("データの取得に失敗しました。", error);
+      } finally {
+        setLoading(false);
+      }
     };
 
     fetcher();
   }, []);
+
+  if (loading) return <p>読み込み中...</p>;
+
+  if (!posts) return <p>記事が見つかりません</p>;
 
   return (
     <div>
